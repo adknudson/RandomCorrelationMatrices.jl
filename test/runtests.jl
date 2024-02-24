7d0275d4de25b08e6e2fc8abf01efab61753f2a1
aknudson <alexk.706@gmail.com>
2024-02-23 17:12:35 -0800

    updated tests to test minimum interface

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,50 +1,39 @@
-using Test
-using LinearAlgebra: cholesky, diag, Diagonal, Hermitian
-using Printf: @printf
-using Random
-using Statistics: mean, median, std
+using Test, Aqua
 using RandomCorrelationMatrices
+using LinearAlgebra: cholesky, diag, Diagonal, Hermitian
+using Statistics: mean, median, std
 
-# This is not a numerical test. The code just generates some examples.
-# However, if it fails to run, thats a good sign something is wrong.
-Random.seed!(12345)
-println("Random correlation matrix")
-n = 4
-for η in [2, 8, 32, 128]
-    r = RandomCorrelationMatrices.randcormatrix(n, η)
-    @show η
-    for i in 1:n
-        for j in 1:n
-            @printf("%5.2f ", r[i,j])
-        end
-        println()
-    end
 
-    ranges = Float64[]
-    for s in 1:1000
-        r = RandomCorrelationMatrices.randcormatrix(n, η)
-        r0 = r - Matrix(Diagonal((diag(r))))
-        push!(ranges, maximum(vec(r0)) - minimum(vec(r0)))
-    end
-    @show mean(ranges)
-    @show median(ranges)
+Aqua.test_all(RandomCorrelationMatrices; ambiguities=false)
+Aqua.test_ambiguities(RandomCorrelationMatrices)
+
+
+@testset "Random Correlation Matrix" begin
+    # Test the basic interface
+    @test_nowarn randcormatrix(10, 4)
 end
 
 
-@testset "Random covariance matrix" begin
-    Random.seed!(12345)
-    σ = [100, 200, 300, 400, 500]
-    Σ = RandomCorrelationMatrices.randcovmatrix(5, 10, σ)
-    L = cholesky(Hermitian(Σ)).L
+@testset "Random Covariance Matrix" begin
+    s = [100, 200, 300, 400, 500]
+    n = length(s)
+    η = 10
+
+    @test_nowarn randcovmatrix(n, η, s)
+
+    Σ = randcovmatrix(n, η, s)
+    chol = cholesky(Hermitian(Σ))
     samples = 100_000
     results = zeros(5, samples)
-    for i in 1:samples
-        results[:,i] = L*randn(5)
+    for i in axes(results, 2)
+        results[:,i] = chol.L * randn(5)
     end
+
+    # TODO: what property is this testing?
     means = mean(results, dims=2)
     @test all(abs.(means) .<= 5)
+
+    # TODO: what property is this testing?
     stddevs = std(results, dims=2)
-    @test all(abs.(stddevs - σ) .<= 5)
-    @show σ
-    @show round.(stddevs, digits=1)
+    @test all(abs.(stddevs - s) .<= 5)
 end
